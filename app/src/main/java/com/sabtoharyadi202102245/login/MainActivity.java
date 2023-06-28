f315d08c6192d57ab5e70927277dbbe2999083ce
SabtoHaryadi0 <harryjt858@gmail.com>
2023-06-28 09:22:56 +0700

    push

--- app/src/main/java/com/sabtoharyadi202102245/login/MainActivity.java
+++ app/src/main/java/com/sabtoharyadi202102245/login/MainActivity.java
@@ -14,20 +14,6 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import cz.msebera.android.httpclient.Header;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-
-import cz.msebera.android.httpclient.Header;
-
 public class MainActivity extends AppCompatActivity
 {
     private Button _loginButton;
@@ -54,34 +40,31 @@ public class MainActivity extends AppCompatActivity
 
                 _id = _idEditText.getText().toString();
                 _password = _passwordEditText.getText().toString();
-
-                _url = "http://stmikpontianak.net/011100862/login.php?id=" + "&password" + _password;
+                _url = "https://stmikpontianak.net/011100862/login.php?id=" + _id + "&password=" + _password;
 
                 asyncHttpClient = new AsyncHttpClient();
                 asyncHttpClient.get(_url, new AsyncHttpResponseHandler() {
                     @Override
-                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody)
-                    {
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                         String hasil = new String(responseBody);
+//                        Toast.makeText(getApplicationContext(), new String(responseBody), Toast.LENGTH_SHORT).show();
 
-                        if (!hasil.equals("[{\"idCount\":\"1\"}]"))
-                        {
-                            Toast.makeText(getApplicationContext(), "ID dan Password anda tidak cocok.", Toast.LENGTH_LONG).show();
+                        if (!hasil.equals("[{\"idCount\":\"1\"}]")) {
+                            Toast.makeText(getApplicationContext(), "ID dan Password anda tidak cocok.", Toast.LENGTH_SHORT).show();
                             return;
                         }
 
                         Toast.makeText(getApplicationContext(), "Selamat datang, " + _id, Toast.LENGTH_SHORT).show();
-
                         _menuIntent = new Intent(getApplicationContext(), MenuActivity.class);
                         startActivity(_menuIntent);
                     }
 
                     @Override
-                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error)
-                    {
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
                         Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
+
             }
         });
     }
